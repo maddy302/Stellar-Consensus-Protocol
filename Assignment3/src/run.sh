e01ff840733d8de695ecafa863598cf6666865ca
Madhukar <bsm4u2722#gmail.com>
2018-12-10 16:47:29 -0800

    second commit

--- Assignment3/src/run.sh
+++ Assignment3/src/run.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+# #!/bin/sh
 
-python udp_server.py 5000  sleep 5s &
-python udp_server.py 5001 sleep 5s &
-python udp_server.py 5002 sleep 5s &
-python udp_server.py 5003 sleep 5s &
-python test_client.py
-# pid_list = $(ps -ef | grep python)
-# for pid in pid_list; do
-#     echo $pid
-# done
+# python udp_server.py 5000  sleep 5s &
+# python udp_server.py 5001 sleep 5s &
+# python udp_server.py 5002 sleep 5s &
+# # python udp_server.py 5003 sleep 5s &
+# python test_client.py
+# # pid_list = $(ps -ef | grep python)
+# # for pid in pid_list; do
+# #     echo $pid
+# # done
